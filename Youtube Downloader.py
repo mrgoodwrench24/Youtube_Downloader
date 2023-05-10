332ebd4f574b0282288e9bf18e5a2fcf51120eac
Michael White <michaeltroywhite24@gmail.com>
2023-05-10 11:39:24 -0600

    5.10.23.1

--- Youtube Downloader.py
+++ Youtube Downloader.py
@@ -3,20 +3,20 @@ import os
 from pytube import Playlist, YouTube
 from pytube.exceptions import VideoUnavailable
 
-1
 choice = "0"
 
-def Download_Video():
+
+def download_video():
     link = input("What is the url?\n")
     type = input("Type 1 for Video and Type 2 for Music")
     if type == '1':
         file_extension = '.mp4'
-        youtubeObject = YouTube(link)
-        youtubeObject = youtubeObject.streams.get_highest_resolution()
+        youtube_object = YouTube(link)
+        youtube_object = youtube_object.streams.get_highest_resolution()
     elif type == '2':
         file_extension = '.mp3'
-        youtubeObject = YouTube(link)
-        youtubeObject = youtubeObject.streams.filter(only_audio=True).get_audio_only()
+        youtube_object = YouTube(link)
+        youtube_object = youtube_object.streams.filter(only_audio=True).get_audio_only()
     else:
         print("Invalid response.  Type 1 for Video and Type 2 for Music")
 
@@ -26,14 +26,14 @@ def Download_Video():
     current_directory = os.getcwd()
 
     try:
-        print("Downloading " + youtubeObject.title + "\n\n")
-        youtubeObject.download(output_path=current_directory,filename=filename)
+        print("Downloading " + youtube_object.title + "\n\n")
+        youtube_object.download(output_path=current_directory,filename=filename)
         print(filename + " Download is completed successfully")
     except:
         print("An error has occurred")
 
 
-def Download_Playlist():
+def download_playlist():
     link = input("What is the playlist url?\n")
     p = Playlist(link)
     for link in p.video_urls:
@@ -47,14 +47,13 @@ def Download_Playlist():
             video.streams.get_highest_resolution().download()
             print(video.title + " Download is completed successfully")
 
-
 while choice != "q":
     choice = input("Youtube Downloader\nWelcome!  Choose a number then hit enter\n1. Download Single Youtube video\n2. Download Playlist\n q to quit\n")
     if choice == "1":
         os.system('cls')
-        Download_Video()
+        download_video()
     elif choice == "2":
-        Download_Playlist()
+        download_playlist()
     elif choice == "q":
         break
     else:
